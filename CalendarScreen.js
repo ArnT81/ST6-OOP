72ae215300ba5e8d5aa7bb0d78a7057788862bce
muzenjakkevin <muzenjak57@gmail.com>
2020-01-13 12:11:00 +0100

    time to push motherfukkers

--- CalendarScreen.js
+++ CalendarScreen.js
@@ -2,11 +2,9 @@ class CalendarScreen extends Screen {
   constructor() {
     super();
     this.header();
-    // new GetTime();
     this.initTime();
-    this.dayRef();
-    this.monthRef();
-    // this.show();
+    this.initDays();
+    this.initMonths();
     this.initCalendar();
   }
 
@@ -25,50 +23,41 @@ class CalendarScreen extends Screen {
   initTime() {
     this.d = new Date();
     this.date = this.d.getDate(this.d.getDate()+1);
-    // console.log(this.d);
-    // console.log(this.date);
   }
 
-  dayRef() {
-    this.dayRef = new Array;
-    this.dayRef[0] = 'Mon';
-    this.dayRef[1] = 'Tue';
-    this.dayRef[2] = 'Wed';
-    this.dayRef[3] = 'Thu';
-    this.dayRef[4] = 'Fri';
-    this.dayRef[5] = 'Sat';
-    this.dayRef[6] = 'Sun';
-    this.day = this.dayRef[this.d.getDay()];
+  initDays() {
+    this.days = ['Mon','Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun'];
+    this.day = this.days[this.d.getDay()];
   }
 
-  monthRef() {
-    this.monthRef = new Array;
-    this.monthRef[0] = '1';
-    this.monthRef[1] = '2';
-    this.monthRef[2] = '3';
-    this.monthRef[3] = '4';
-    this.monthRef[4] = '5';
-    this.monthRef[5] = '6';
-    this.monthRef[6] = '7';
-    this.monthRef[7] = '8';
-    this.monthRef[8] = '9';
-    this.monthRef[9] = '10';
-    this.monthRef[10] = '11';
-    this.monthRef[11] = '12';
-    this.month = this.monthRef[this.d.getMonth()];
+  initMonths() {
+    let months = ['1', '2','3','4','5','6','7','8','9','10', '11', '12'];
+    this.month = months[this.d.getMonth()];
   }
-
-  // show() {
-  //   this.show = this.day + '<br>' + this.date + '/' + this.month;
-  //   // console.log(this.show);
-  // }
 
   initCalendar() {
-    for (var i = 0; i < this.dayRef.length; this.date++ , i++) {
-      this.myDay = document.createElement('div');
-      this.element.appendChild(this.myDay);
-      this.myDay.innerHTML = this.dayRef[i] + '<br>' + this.date + '/' + this.month;
-      this.myDay.className = 'myDay';
+    for (let i = 0; i < this.days.length; this.date++ , i++) {
+      this.dayRef = document.createElement('div');
+      this.dayRef.className = 'myDay';
+      
+      this.element.appendChild(this.dayRef);
+      
+      this.showDayAndDate = document.createElement('p');
+      this.showDayAndDate.className = 'days-and-date';
+      this.showDayAndDate.innerHTML = this.days[i] + '<br>' + this.date + '/' + this.month;
+      this.dayRef.appendChild(this.showDayAndDate);
+
+      this.hoursRef = document.createElement('div');
+      this.hoursRef.className = 'hours-div';
+      this.dayRef.appendChild(this.hoursRef);
+
+      for (let i = 0; i < 24; i++) {
+        
+        this.hoursLineRef = document.createElement('hr');
+        this.hoursLineRef.className = 'hours-line';
+  
+        this.hoursRef.appendChild(this.hoursLineRef);
+      }
     }
   }
 };
